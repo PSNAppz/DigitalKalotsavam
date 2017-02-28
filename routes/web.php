00ced7d30ca7925ad7b32cb97d0984239a4b4cf7
Markandeya <vasalpha@gmail.com>
2017-02-28 23:02:39 +0530

    Creates Student migration

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['except' => ['register']]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin','AdminController@index')->name('admin');
