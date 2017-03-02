4130af61d54e5d4c77f9550ebf00eaef9f831560
hkk710 <hkk710@gmail.com>
2017-03-02 22:45:23 +0530

    welcome page updated

---
+++ app/Http/Controllers/webteamController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class webteamController extends Controller
+{
+    public function index() {
+        return view('webteam.webteam');
+    }
+}
