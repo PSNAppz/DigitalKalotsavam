f37af43107caf34afc8a6b0fd27a53eac66ded17
Markandeya <vasalpha@gmail.com>
2017-03-08 21:21:16 +0530

    Seeder patch

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,31 +16,36 @@ class UsersTableSeeder extends Seeder
           'id' => '1',
           'name' => 'Amritamayi',
           'email' => 'xyz@xyz.com',
-          'password' => '$2y$10$C.vFgshEEZhaql0OYMAnP.UxNOXqi71CYN/nJj5r/4UwXZH.8YdKq'
+          'password' => '$2y$10$C.vFgshEEZhaql0OYMAnP.UxNOXqi71CYN/nJj5r/4UwXZH.8YdKq',
+          'role' => 0
         ],
         [
           'id' => '2',
           'name' => 'Jyothirmayi',
           'email' => 'abc@abc.com',
-          'password' => '$2y$10$OpyKIgD55M/YxTPuVVRq4.EpSKirzkhIb5di3Olfqv1rUJe2USuz.'
+          'password' => '$2y$10$OpyKIgD55M/YxTPuVVRq4.EpSKirzkhIb5di3Olfqv1rUJe2USuz.',
+          'role' => 0
         ],
         [
           'id' => '3',
           'name' => 'Anandamayi',
           'email' => 'test@test.com',
-          'password' => '$2y$10$eq7K0zus6uvD1gn2mfhQWecf5ZBOc5kxvl4n6E.evW1FNy/aSGKHC'
+          'password' => '$2y$10$eq7K0zus6uvD1gn2mfhQWecf5ZBOc5kxvl4n6E.evW1FNy/aSGKHC',
+          'role' => 0
         ],
         [
           'id' => '4',
           'name' => 'Chinmayi',
           'email' => 'noreply@noreply.com',
-          'password' => '$2y$10$10cPaEwhvBFhlP.PC/dXDuWzALivNj/0ys67T2R212qsADsf3IyuS'
+          'password' => '$2y$10$10cPaEwhvBFhlP.PC/dXDuWzALivNj/0ys67T2R212qsADsf3IyuS',
+          'role' => 0
         ],
         [
           'id' => '10',
           'name' => 'Admin',
           'email' => 'admin@admin.com',
-          'password' => '$2y$10$wmtXwrCv02GMB8DJeNSgcu9fo7enotQX.rhHjbYkMvAzRpDayraSq'
+          'password' => '$2y$10$wmtXwrCv02GMB8DJeNSgcu9fo7enotQX.rhHjbYkMvAzRpDayraSq',
+          'role' => 2
         ]
       ]);
     }
